0d193feadbaac9a96c34f51e38cd885e447b9f5e
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-28 21:51:55 +0000

    Problems with api success should be solved

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,7 @@ router.register('missions', views.SpaceMissionsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('missions/company/<company_name>/', views.SpaceMissionByCompany.as_view())
+    path('missions/company/<company_name>/', views.SpaceMissionByCompany.as_view()),
+    path('missions/name/<mission_name>/', views.SpaceMissionByMissionName.as_view()),
+    path('missions/success/', views.SuccessMissions.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
@@ -9,16 +10,55 @@ from .serializers import SpaceMissionsSerializer
 
 class SpaceMissionsViewSet(viewsets.ModelViewSet):
     '''
-    API endpoint to get all space missions
+    API endpoint to retrieve all space missions
     '''
     queryset = SpaceMissions.objects.all()
     serializer_class = SpaceMissionsSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 class SpaceMissionByCompany(ListAPIView):
+    '''
+    API endpoint to retrieve space missions by company name
+    '''
     serializer_class = SpaceMissionsSerializer
     
     def get_queryset(self):
         company_name = self.kwargs['company_name']
         return SpaceMissions.objects.filter(company__icontains=company_name)
+
+class SpaceMissionByMissionName(ListAPIView):
+    '''
+    API endpoint to retrieve space missions by mission name
+    '''
+    serializer_class = SpaceMissionsSerializer
+
+    def get_queryset(self):
+        mission_name = self.kwargs['mission_name']
+        return SpaceMissions.objects.filter(mission__icontains=mission_name)
+
+
+class SuccessMissions(ListAPIView):
+    queryset = SpaceMissions.objects.filter(success=True)
     
+    def list(self, request):
+        success_misions = self.get_queryset()
+        serializer = SpaceMissionsSerializer(success_misions)
+        return Response(serializer.data)
+
+# class SuccessMissions(ListAPIView):
+#     '''
+#     API endpoint to retrieve all succcesful missions
+#     '''
+#     serializer_class = SpaceMissionsSerializer
+
+#     def get_queryset(self):
+#         return SpaceMissions.objects.filter(success=True)
+    
+# class UnsuccessMissions(ListAPIView):
+#     '''
+#     API endpoint to retrieve all unsucccesful missions
+#     '''
+#     serializer_class = SpaceMissionsSerializer
+
+#     def get_queryset(self):
+#         return SpaceMissions.objects.filter(success=False)
