24c39a717fde784c18423ca7284039a6d2cacceb
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-30 08:42:13 +0000

    Settings with new font

--- api/views.py
+++ api/views.py
@@ -1,36 +1,38 @@
 from rest_framework import viewsets
-
 from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
 
 from .models import SpaceMissions
 from .serializers import SpaceMissionsSerializer
 
+
 class SpaceMissionsViewSet(viewsets.ModelViewSet):
-    '''
+    """
     API endpoint to retrieve all space missions
-    '''
+    """
+
     queryset = SpaceMissions.objects.all()
     serializer_class = SpaceMissionsSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class SpaceMissionByCompany(ListAPIView):
-    '''
+    """
     API endpoint to retrieve space missions by company name
-    '''
+    """
+
     serializer_class = SpaceMissionsSerializer
-    
+
     def get_queryset(self):
         company_name = self.kwargs['company_name']
         return SpaceMissions.objects.filter(company__icontains=company_name)
 
+
 class SpaceMissionByMissionName(ListAPIView):
-    '''
+    """
     API endpoint to retrieve space missions by mission name
-    '''
+    """
+
     serializer_class = SpaceMissionsSerializer
 
     def get_queryset(self):
@@ -39,11 +41,11 @@ class SpaceMissionByMissionName(ListAPIView):
 
 
 class SuccessMissions(ListAPIView):
-    
     serializer_class = SpaceMissionsSerializer
 
     def get_queryset(self):
         return SpaceMissions.objects.filter(success=True)
+
 
 # class SuccessMissions(ListAPIView):
 #     '''
@@ -53,7 +55,7 @@ class SuccessMissions(ListAPIView):
 
 #     def get_queryset(self):
 #         return SpaceMissions.objects.filter(success=True)
-    
+
 # class UnsuccessMissions(ListAPIView):
 #     '''
 #     API endpoint to retrieve all unsucccesful missions
